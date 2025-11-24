8e0f4a8edc124510c38be9d14cd1c5ffb305ec7b
diogoflduarte <diogo.fl.duarte@gmail.com>
2025-11-24 14:27:30 +0000

    undetected chromedriver. to be tested

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -1,4 +1,4 @@
-
+import undetected_chromedriver as uc
 from selenium import webdriver  # to control the browser
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -98,13 +98,13 @@ class ChromiumHandler():
         self.options    = ChromeOptions()
         # important: the following line suppresses the google chromium banner
         # saying that the browser is being controlled externally
-        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
         self.options.add_experimental_option("prefs", {
                     "profile.default_content_setting_values.notifications": 1,  # 1=allow, 2=block, 0=ask
                     "profile.managed_default_content_settings.notifications": 1,
                     "credentials_enable_service": False,
                     "profile.password_manager_enabled": False})
-        self.browser = webdriver.Chrome(service=self.service, options=self.options)
+        self.browser = uc.Chrome(service=self.service, options=self.options)
         # self.browser is the important 'output'
     def open_url(self, url):
         self.browser.get(url)
