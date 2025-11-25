2f717989e77a81980ff87980cca1469682b6637d
diogoflduarte <diogo.fl.duarte@gmail.com>
2025-11-25 16:37:25 +0000

    better cleanup. flexible owl tested

--- prr_experiments/launch_cyclesix.sh
+++ prr_experiments/launch_cyclesix.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")"
 source ~/.bashrc
 pkill mediamtx
 pkill evince
+pkill chrome
 rm -rf ~/.cache/evince
 cd /home/participant/aixlab/orchestra
 source /home/participant/aixlab/orchestra/.venv/bin/activate && python /home/participant/aixlab/orchestra/prr_experiments/cyclesix.py
